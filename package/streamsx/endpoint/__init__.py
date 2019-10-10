c11315571ccca1fb84d63bf75bf60af20f60931e
markheger <mark-oliver.heger@de.ibm.com>
2019-10-10 13:02:05 +0000

    version bump v0.5.0

--- package/streamsx/endpoint/__init__.py
+++ package/streamsx/endpoint/__init__.py
@@ -29,7 +29,7 @@ A simple example of a Streams application that provides an endpoint for json inj
 
 """
 
-__version__='0.4.0'
+__version__='0.5.0'
 
 __all__ = ['download_toolkit', 'inject', 'expose']
 from streamsx.endpoint._endpoint import download_toolkit, inject, expose
