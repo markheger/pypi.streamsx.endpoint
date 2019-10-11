76289127836cbe1d005c6d72a3df1836e8ec5a1b
markheger <mark-oliver.heger@de.ibm.com>
2019-10-11 13:16:04 +0000

    resolves #12

--- package/streamsx/endpoint/__init__.py
+++ package/streamsx/endpoint/__init__.py
@@ -6,7 +6,12 @@
 Overview
 ++++++++
 
-Provides functions to add Streams operators acting as communication endpoints for your Streams application.
+For details of implementing applications in Python
+for IBM Streams including IBM Cloud Pak for Data:
+
+  * `streamsx package documentation <https://streamsxtopology.readthedocs.io/en/stable>`_
+
+This package exposes SPL operators in the `com.ibm.streamsx.inetserver <https://ibmstreams.github.io/streamsx.inetserver/>`_ toolkit as Python methods.
 
 
 Sample
